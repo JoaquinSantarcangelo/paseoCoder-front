4a3656c912d565c333d7a24567328233ffb0df01
Joaquin Santarcangelo <joaquinsantarcangelo@gmail.com>
2021-02-15 10:06:23 -0300

    Cart modal

--- pages/tiendas/[tienda]/index.js
+++ pages/tiendas/[tienda]/index.js
@@ -1,11 +1,7 @@
 import React from "react";
-import Tienda from "../../../components/containers/Tienda";
+
 const index = () => {
-  return (
-    <div className="tienda">
-      <Tienda />
-    </div>
-  );
+  return <div className="tienda">Tienda</div>;
 };
 
 export default index;
